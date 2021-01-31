fcacf01dfb9608dbdba0d7ee38360d0db02640aa
daisychelangat <daisyrop2000@gmail.com>
2021-02-01 01:16:04 +0300

    created quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
