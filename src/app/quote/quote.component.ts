842a0fe0afc60ff45c74bbb537faffd78d58cb77
daisychelangat <daisyrop2000@gmail.com>
2021-02-01 01:44:45 +0300

    Used input data binding to display author name after each quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import {Quote  } from "../quote";
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+quotes:Quote[]=[
+  {id:1,quote:'"Be yourself; everyone else is already taken."',author:'Oscar Wilde'},
+];
+@Input() quote: Quote;
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit() {
+}
 
 }
