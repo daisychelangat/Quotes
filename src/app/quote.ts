929974d599d77c463fb90f36f42f9b8f872dd32d
daisychelangat <daisyrop2000@gmail.com>
2021-02-01 11:47:56 +0300

    used a date pipe to make the dates simpler and user friendly

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,11 +1,7 @@
 export class Quote {
-    id:number;
-    quote:string;
-    author:string;
-    public upvotes : number;
-    public downvotes :number;
-    constructor(public id:number,public quote:string,public author:string){
-      this.upvotes=0;
-      this.downvotes=0;
-    }
-      }
+  public upvotes : number;
+  public downvotes :number;
+  constructor(public id:number,public quote:string,public author:string,public createDate: Date){
+    this.upvotes=0;
+    this.downvotes=0;
+  }}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, } from '@angular/core';
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {Quote  } from "../quote";
 @Component({
   selector: 'app-quote',
@@ -8,29 +7,7 @@ import {Quote  } from "../quote";
 })
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
-  {id:1,quote:'"Love the life you live, live the life you love."',author:'Bob Marley'},
-  {id:2,quote:'"Difficulties in your life do not come to destroy you but to help you realise your hidden potential and power. Let difficulties know that you too are difficult."',author:'Avul Pacir Zainulabidin Abdul Kalam'},
-  {id:1,quote:'"Never bend your head. Always hold it high. Look the world straight in the eye."',author:'Helen Keller '},
+   new Quote(1,'"Love the life you live, live the life you love."','Bob Marley',new Date(2021,2,1)),
+  new Quote(2,'"In whatever you do,do it to your best"','Henry Dru',new Date(2021,1,30)),
+
 ];
-@Output() toDelete = new EventEmitter<boolean>();
-
-quoteComplete(complete:boolean){
-  this.toDelete.emit(complete);
-}
-deleteQuote(toDelete, index){
-   if (toDelete) {
-     this.quotes.splice(index,1);
-   }
- }
- upvote(i){
-      this.quotes[i].upvotes+=1
-  }
-downvote(i){
-    this.quotes[i].downvotes+=1
-  }
-
-constructor() { }
-
-ngOnInit() {
-}
-}
